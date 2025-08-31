99c8512ca9c7b5956f0084e0694109d155fe4d07
rudolfp <capacopx@gmail.com>
2025-08-31 17:13:46 +0200

    in progress

--- sound.js
+++ sound.js
@@ -1,8 +1,28 @@
 console.log("hello");
 
+class PianoKey {
+    constructor (htmlDiv, soundFile) {
+        this.htmlDiv = htmlDiv;
+        this.soundFile = soundFile;
+    }
+
+    trigger () {
+        //this.htmlDiv.dispatchEvent(new Event("click"));
+        const event = new Event("mouseenter", {
+            bubbles: true,    // optional, wenn Event nach oben propagieren soll
+            cancelable: true
+        });
+        this.htmlDiv.dispatchEvent(event);
+
+        this.soundFile.currentTime = 0;
+        this.soundFile.play();
+    }
+}
+
 class KeyHandler {
     static keysPressed = new Set();
 
+    /*aaa
     static noteMap = {
         "a": new Audio("audio/c-sine.mp3"),
         "s": new Audio("audio/d-sine.mp3"),
@@ -11,6 +31,11 @@ class KeyHandler {
         "j": new Audio("audio/kick.mp3"),
         "k": new Audio("audio/snare.mp3"),
         "l": new Audio("audio/hat.mp3")
+    }
+    */
+
+    static noteMap = {
+        "a": new PianoKey(document.getElementById("c-sine"), new Audio("audio/c-sine.mp3"))
     }
 
     static onKeyDown (key) {
@@ -23,10 +48,7 @@ class KeyHandler {
         }
 
         KeyHandler.keysPressed.add(key);
-
-        const audio = KeyHandler.noteMap[key];
-        audio.currentTime = 0;
-        audio.play();
+        KeyHandler.noteMap[key].trigger();
     }
     
     static onKeyUp (key) {
@@ -41,3 +63,8 @@ window.addEventListener("keydown", (event) => {
 window.addEventListener("keyup", (event) => {
     KeyHandler.onKeyUp(event.key);
 });
+
+const cKey = document.getElementById("c-sine");
+console.log(cKey)
+cKey.addEventListener("click", ()=>{console.log("clicked")})
+cKey.addEventListener("mouseenter", () => {console.log("mouseenter")})
